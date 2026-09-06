afa25ad21468c1d7977a164f20a31afed6936983
mkochinyan <marinakochinyann@gmail.com>
2026-09-06 21:25:16 +0400

    idhasdisha

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import string
+
 
 df = pd.read_csv("spam.csv",encoding='latin-1')
 print(df['Class'].value_counts(normalize=True))
 
 df['Length'] = df['Message'].str.len()
-print(df.head())
+

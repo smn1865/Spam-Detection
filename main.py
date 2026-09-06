ac434d9815bb3455ed1b3fd435645028a817c33b
mkochinyan <marinakochinyann@gmail.com>
2026-09-06 21:52:49 +0400

    the great cleansing

--- main.py
+++ main.py
@@ -11,8 +11,3 @@ print(df['Class'].value_counts(normalize=True))
 df['Length'] = df['Message'].str.len()
 
 
-df = pd.read_csv('spam.csv', encoding = 'latin-1')
-print(df.describe())
-
-df = df['Message'].str.lower().str.split()
-print(df.head())
